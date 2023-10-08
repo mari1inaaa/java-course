60e35b4df1f412a09e66307bb17c72f9f0a0255f
Marina <marybebra02@gmail.com>
2023-10-08 18:47:21 +0700

    fixed errors

--- src/main/java/edu/hw1/Task8.java
+++ src/main/java/edu/hw1/Task8.java
@@ -7,7 +7,8 @@ public class Task8 {
 
     @SuppressWarnings("Magic Number")
     public static boolean knightBoardCapture(int[][] board) {
-        int[][] moves = {{-2, -1}, {-2, 1}, {2, 1}, {2, -1}, {1, 2}, {1, -2}, {-1, 2}, {-1, -2}};
+        final int M = -2;
+        int[][] moves = {{M, -1}, {M, 1}, {2, 1}, {2, -1}, {1, 2}, {1, M}, {-1, 2}, {-1, M}};
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
                 if (board[i][j] == 1) {
